e8654b2b2193e51e0572a0779bc6a6ef7db5ece5
Chris Canterbury <ranchodeluxeco@Chriss-MacBook-Pro.local>
2019-03-29 02:09:51 -0500

    moves to v0.1.1

--- rdc-business-hours.php
+++ rdc-business-hours.php
@@ -7,7 +7,7 @@
  * Author URI:      https://ranchodeluxe.dev
  * Text Domain:     rdc-business-hours
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         0.1.1
  * Github Repository: ranchodeluxemedia/rdc-business-hours
  * Github Plugin URI: ranchodeluxemedia/rdc-business-hours
  *
